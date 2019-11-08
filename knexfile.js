6f99c855487753fc2d92c0b16d3caff79aec80f9
RichardEsquivel <rlesquivel@gmail.com>
2019-11-08 11:06:07 -0600

    Made knex and bootstrap migration file

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,19 @@ module.exports = {
 		client: 'sqlite3',
 		connection: {
 			filename: './dev.sqlite3'
-		}
+		},
+		migrations: {
+			directory: "data/migrations"
+		},
+		seeds: {
+			diretory: "./data/seeds"
+		},
+		useNullAsDefault: true,
+		pool: {
+			afterCreate: (conn, done) => {
+				conn.run('PRAGMA foreign_keys=ON', done);
+			},
+		},
 	},
 
 
