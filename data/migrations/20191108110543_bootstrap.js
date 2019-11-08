d692c286c88921464e8ac58c98cf0f20e3256383
RichardEsquivel <rlesquivel@gmail.com>
2019-11-08 12:02:05 -0600

    Created projects.db with knex file

--- data/migrations/20191108110543_bootstrap.js
+++ data/migrations/20191108110543_bootstrap.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
-  
+//creating projects table initially
+exports.up = function (knex) {
+	return knex.schema.createTable('projects', tbl => {
+		tbl.increments()
+		tbl.varchar('name', 255)
+			.notNullable();
+		tbl.varchar('description', 255);
+		tbl.boolean("complete").defaultTo(false);
+	})
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+	return knex.schema.dropTableIfExists('projects')
 };
